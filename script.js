727a9c23f6e42193011f6515d57fe6025498c61c
arthurbcg <arthurbcg16@gmail.com>
2023-08-02 18:28:08 -0300

    finish project

---
+++ script.js
@@ -0,0 +1,87 @@
+let areas = {
+    a: null,
+    b: null,
+    c: null
+}
+
+document.querySelectorAll(".item").forEach(item => {
+    item.addEventListener('dragstart', dragStart)
+    item.addEventListener('dragend', dragEnd)
+})
+
+document.querySelectorAll(".area").forEach(area => {
+    area.addEventListener('dragover', dragOver)
+    area.addEventListener('dragleave', dragLeave)
+    area.addEventListener('drop', drop)
+})
+
+document.querySelector(".neutralArea").addEventListener('dragover', dragOverNeutral)
+document.querySelector(".neutralArea").addEventListener('dragleave', dragLeaveNeutral)
+document.querySelector(".neutralArea").addEventListener('drop', dropNeutral)
+
+//functions -> item
+function dragStart(e) {
+    e.currentTarget.classList.add("dragging")
+}
+
+function dragEnd(e) {
+    e.currentTarget.classList.remove("dragging")
+}
+
+//functions -> areas
+function dragOver(e) {
+    if(e.currentTarget.querySelector(".item") === null) {
+        e.preventDefault()
+        e.currentTarget.classList.add("hover")
+    }
+}
+
+function dragLeave(e) {
+    e.currentTarget.classList.remove("hover")
+}
+
+function drop(e) {
+    e.currentTarget.classList.remove("hover")
+
+    if(e.currentTarget.querySelector(".item") === null) {
+        let dragItem = document.querySelector(".item.dragging")
+        e.currentTarget.appendChild(dragItem)
+        updateAreas()
+    }
+}
+
+//functions -> neutral area
+function dragOverNeutral(e) {
+    e.preventDefault()
+    e.currentTarget.classList.add("hover")
+}
+
+function dragLeaveNeutral(e) {
+    e.currentTarget.classList.remove("hover")
+}
+
+function dropNeutral(e) {
+    e.currentTarget.classList.remove("hover")
+    let dragItem = document.querySelector(".item.dragging")
+    e.currentTarget.appendChild(dragItem)
+    updateAreas()
+}
+
+//functions - logica
+function updateAreas(){
+    document.querySelectorAll(".area").forEach(area => {
+        let nameArea = area.getAttribute("data-name")
+
+        if (area.querySelector(".item") !== null) {
+            areas[nameArea] = area.querySelector(".item").innerHTML
+        } else {
+            areas[nameArea] = null
+        }
+    })
+
+    if (areas.a === "2" && areas.b === "3" && areas.c === "1") {
+        document.querySelector(".areas").classList.add("correct")
+    } else {
+        document.querySelector(".areas").classList.remove("correct")
+    }
+}
